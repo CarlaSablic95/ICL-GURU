647a71fafac641f010756b263987fbecf16d20b0
CarlaSablic95 <carlasablic95@gmail.com>
2024-05-21 12:35:54 -0300

    add(-): Creación de la página de login

---
+++ src/assets/pages/Login.jsx
@@ -0,0 +1,13 @@
+const Login = () => {
+    return (
+        <main>
+            <section>
+                <form action="">
+                    
+                </form>
+            </section>
+        </main>
+    )
+}
+
+export default Login;
