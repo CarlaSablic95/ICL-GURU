5115fe9bfdbdf47e5b5be5e1b4963184631fdd37
CarlaSablic95 <carlasablic95@gmail.com>
2024-05-24 01:08:32 -0300

    add(-): Creación de la página de Login

--- src/assets/pages/Login.jsx
+++ src/assets/pages/Login.jsx
@@ -1,101 +1,104 @@
-import { useFormik } from "formik";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
 import Button from "../components/Button/Button";
-
+import EyeOff from "/icons/eye-off.svg";
+import EyeOn from "/icons/vite.svg";
 
 const Login = () => {
-    const validate = values => {
-        const errors = {};
+  const [showPassword, setShowPassword] = useState(false);
 
-        if(!values.username) {
-            errors.username = "This field is required";
-        }
+  const handleShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
 
-        if(!values.email) {
-            errors.email = "This field is required";
-        } else if(!/^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/i.test(values.email)) {
-            errors.email = "Invalid email address";
-        }
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => {
+    const { username, email, password } = data;
+        console.log("Username:", username);
+        console.log("Email:", email);
+        console.log("Password:", password);
+  };
 
-        if(!values.password) {
-            errors.password = "This field is required";
-        } else if(values.password.length < 8) {
-            errors.password = "Must be 8 characters";
-        }
+  const isFormInvalid = !!errors.username || !!errors.email || !!errors.password;
 
-        return errors;
-    };
-
-    const formik = useFormik({
-        initialValues: {
-            username: "",
-            email: "",
-            password: ""
-        },
-
-        validate,
-
-        onSubmit: values => {
-            console.log(JSON.stringify(values, null, 2));
-        }
-    });
-
-    return (
-        <main>
-            <section>
-                <h1 className="text-uppercase">Icl Guru</h1>
-                <form onSubmit={ formik.handleSubmit }>
-                    <div className="form-floating mb-3">
-                    <input 
-                        id="username"
-                        name="username"
-                        type="text"
-                        className="form-control rounded-5"
-                        onChange={ formik.handleChange }
-                        value={ formik.values.username }
-                        placeholder="Username"
-                        />
-                        { formik.errors.username ? <small className="text-danger">{ formik.errors.username }</small>  : null }
-                    <label htmlFor="username">Username</label>
-                    </div>
-
-                    <div className="form-floating mb-3">
-                    <input 
-                        id="email"
-                        name="email"
-                        type="email"
-                        className="form-control rounded-5"
-                        onChange={ formik.handleChange }
-                        value={ formik.values.email }
-                        placeholder="Email"
-                        />
-                        { formik.errors.email ? <small className="text-danger">{ formik.errors.email }</small> : null }
-                  <label htmlFor="email">Email</label>
-                  </div>
-
-                  <div className="form-floating mb-3">
-                        <input 
-                            id="password"
-                            name="password"
-                            type="password"
-                            className="form-control rounded-5"
-                            onChange={ formik.handleChange }
-                            value={ formik.values.password }
-                            placeholder="Password"
-                        />
-                        
-                        { formik.errors.password ? <small className="text-danger">{ formik.errors.password }</small> : null }
-
-                    <label htmlFor="password">Password</label>
-                    </div>
-
-                        <div className="">
-                            <Button type="submit" name="Login" />
+  return (
+      <section className="bg-login d-flex  align-items-center">
+        <div className="container">
+            <div className="row justify-content-center">
+                <div className="col-10 col-md-4 bg-fondo d-none d-md-block"></div>
+                    <div className="col-10 col-md-4 px-5 px-md-4 bg-form">
+                        <form onSubmit={handleSubmit(onSubmit)}>
+                            <h1 className="text-uppercase text-center py-4 text-white">Icl Guru</h1>
+                        <div className="form-floating mb-3">
+                            <input
+                            {...register("username", { required: true })}
+                            placeholder="Username"
+                            className="form-control rounded-5 border-0"
+                            id="username"
+                            type="text"
+                            />
+                            {errors.username && (
+                            <small className="text-danger">This field is required</small>
+                            )}
+                            <label htmlFor="username">Username</label>
                         </div>
-                            <a href="/">I forgot my password</a>
-                </form>
-            </section>
-        </main>
-    )
-}
+
+                        <div className="form-floating mb-3">
+                            <input
+                            {...register("email", {
+                                required: true,
+                                pattern: /^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+                            })}
+                            placeholder="Email"
+                            className="form-control rounded-5 border-0"
+                            id="email"
+                            type="email"
+                            />
+                            {errors.email && (
+                            <small className="text-danger">
+                                { errors.email.type === "required" && "This field is required" }
+                                { errors.email.type === "pattern" && "Invalid email format" }
+                            </small>
+
+                            )}
+                            <label htmlFor="email">Email</label>
+                        </div>
+
+                        <div className="form-floating mb-3">
+                            <span
+                            className="position-absolute eye-icon"
+                            onClick={handleShowPassword}
+                            >
+                            <img src={showPassword ? EyeOn : EyeOff} alt="ícono de ojo" />
+                            </span>
+                            <input
+                            {...register("password", { required: true })}
+                            placeholder="Password"
+                            className="form-control rounded-5 border-0"
+                            id="password"
+                            type={showPassword ? "text" : "password"}
+                            />
+
+                            {errors.password && (
+                            <small className="text-danger">This field is required</small>
+                            )}
+                            <label htmlFor="password">Password</label>
+                        </div>
+                        <div className="d-flex justify-content-center">
+                            <Button name="Login" type="submit" bg_color="#00507C" disabled={ isFormInvalid } />
+                        </div>
+
+                        <p className="text-center py-3"><a href="#" className="text-white text-decoration-none">I forgot my password</a></p>
+                        </form>
+                    </div>
+                </div>
+        </div>
+      </section>
+  );
+};
 
 export default Login;
