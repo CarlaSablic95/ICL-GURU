dc49b2eca3a8b951ec9c7b98d403f85ec28bf320
CarlaSablic95 <carlasablic95@gmail.com>
2024-05-26 17:22:07 -0300

    add(-): Creación de la página de cuentas

---
+++ src/assets/pages/Account.jsx
@@ -0,0 +1,10 @@
+const Account = () => {
+    
+    return (
+        <div className="col-10 px-5">
+            <h1 className="text-center">Account</h1>
+        </div>
+    )
+}
+
+export default Account;
