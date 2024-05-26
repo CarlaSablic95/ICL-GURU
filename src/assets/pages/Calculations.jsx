ba5a464093030cf1d9942fcae6b40c13b61bfb90
CarlaSablic95 <carlasablic95@gmail.com>
2024-05-26 17:24:06 -0300

    add(-): Creación de la página de cálculos

---
+++ src/assets/pages/Calculations.jsx
@@ -0,0 +1,10 @@
+const Calculations = () => {
+    
+    return (
+        <div className="col-10 px-5">
+            <h1 className="text-center">Calculations</h1>
+        </div>
+    )
+}
+
+export default Calculations;
