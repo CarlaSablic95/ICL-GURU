4cb423d36b23e23c6f92dd0b63b5019132789e85
CarlaSablic95 <carlasablic95@gmail.com>
2024-05-26 17:31:51 -0300

    fix(-): arreglé el código del botón

--- src/assets/components/Button/Button.jsx
+++ src/assets/components/Button/Button.jsx
@@ -1,8 +1,6 @@
 const Button = ({ name, type, bg_color }) => {
     return ( 
-    <div className="">
         <button type={ type } className="btn rounded-5 text-uppercase mb-3 border-0 text-white btn-login" style={{ backgroundColor: bg_color }}>{ name }</button>
-    </div>
     )
 }
 
