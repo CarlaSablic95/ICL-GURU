0e645d5e033bd8b8c02fd52a6adf92e8b1c2c654
CarlaSablic95 <carlasablic95@gmail.com>
2024-05-26 17:23:25 -0300

    add(-): Creación de la página para agregar pacientes

---
+++ src/assets/pages/AddPatients.jsx
@@ -0,0 +1,10 @@
+const AddPatients = () => {
+    
+    return (
+        <div className="col-10 px-5">
+            <h1 className="text-center">Add Patients</h1>
+        </div>
+    )
+}
+
+export default AddPatients;
