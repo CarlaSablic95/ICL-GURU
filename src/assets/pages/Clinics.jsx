fcda6b998797ed364bfdd5d6897c09d1c5ea4bf8
CarlaSablic95 <carlasablic95@gmail.com>
2024-05-26 17:24:32 -0300

    add(-): Creación de la página de clínicas

---
+++ src/assets/pages/Clinics.jsx
@@ -0,0 +1,10 @@
+const Clinics = () => {
+    
+    return (
+        <div className="col-10 px-5">
+            <h1 className="text-center">Clinics</h1>
+        </div>
+    )
+}
+
+export default Clinics;
