a1560bd4dd43ba15f28a172b29333cbb47b8de9f
CarlaSablic95 <carlasablic95@gmail.com>
2024-05-26 17:25:08 -0300

    add(-): Creación de la página para el panel de gestión

---
+++ src/assets/pages/Dashboard.jsx
@@ -0,0 +1,15 @@
+import Patients from "../components/Patients/Patients";
+
+const Dashboard = () => {
+  return (
+          <div className="col-10 bg-secondary-subtle vh-100 px-0">
+            <div className="bg-white py-3 mb-5">
+              <i className="bi bi-key-fill fs-4 me-2" />
+            </div>
+
+            <Patients />
+          </div>
+  );
+};
+
+export default Dashboard;
