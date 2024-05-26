61db5c408d9c4c7d808ed7135a9d91f4b7ba2ade
CarlaSablic95 <carlasablic95@gmail.com>
2024-05-26 17:26:52 -0300

    add(-): Creación de la página de 'Mi cuenta'

---
+++ src/assets/pages/MyAccount.jsx
@@ -0,0 +1,10 @@
+const MyAccount = () => {
+    
+    return (
+        <div className="col-10 px-5">
+            <h1 className="text-center">My account</h1>
+        </div>
+    )
+}
+
+export default MyAccount;
