55f3bdae09cbd309e008f10ba58067b6d9d6c4ef
OrnellaSablic <ornellasablic@gmail.com>
2024-06-02 18:12:53 -0300

    add(-): Creación de componente Header

--- src/assets/components/Header/Header.jsx
+++ src/assets/components/Header/Header.jsx
@@ -1,9 +1,15 @@
+import Sidebar from "../Sidebar/Sidebar";
+
 const Header = () => {
     return (
-        <header className="py-4 d-flex justify-content-between">
-            <p className='text-white mx-5'>User:</p>
-            <div className="bg-warning mx-5">Logo</div>
-        </header>
+        <>
+            <header className="py-4 d-flex justify-content-between">
+                <p className='text-white mx-5'>User:</p>
+                <div className="bg-warning mx-5">Logo</div>
+            </header>
+
+            <Sidebar />
+        </>
     )
 }
 
