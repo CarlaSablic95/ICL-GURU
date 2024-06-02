1d48f8c4870edd9f128cd1404394ecd19258e01d
CarlaSablic95 <carlasablic95@gmail.com>
2024-06-01 23:44:46 -0300

    add(-): Creación del componente modal para borrar pacientes

---
+++ src/assets/components/Modal/ModalDeletePatient.jsx
@@ -0,0 +1,25 @@
+const ModalDeletePatient = () => {
+    return(
+        <div className="col-10 px-5">
+        <div className="modal fade" id="modalDelete" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div className="modal-dialog modal-dialog-centered">
+            <div className="modal-content px-2 px-md-4">
+                <div className="modal-header pb-2">
+                    <h1>Confirm Submit</h1>
+                    <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div className="modal-body">
+                    <p className="text-center">The patient and all his records will be deleted. Do you wish to continue?</p>
+                </div>
+                <div className="d-flex justify-content-evenly pb-3">
+                    <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">No</button>
+                    <button type="button" className="btn btn-primary border-0" style={{ backgroundColor: "#3DC2DD" }} data-bs-dismiss="modal">Yes</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+    )
+};
+
+export default ModalDeletePatient;
