ab12bf0bae7641f661f80ae6f6d8a98af2fb10c2
OrnellaSablic <ornellasablic@gmail.com>
2024-06-02 18:17:32 -0300

    fix(-): Modificación en la página Dashboard

--- src/assets/pages/Dashboard.jsx
+++ src/assets/pages/Dashboard.jsx
@@ -1,12 +1,9 @@
 import Patients from "../pages/Patients";
 
+
 const Dashboard = () => {
   return (
-          <div className="col-10 bg-secondary-subtle vh-100 px-0">
-            <div className="bg-white py-3 mb-5">
-              <i className="bi bi-key-fill fs-4 me-2" />
-            </div>
-
+          <div className="col-10 vh-100 px-5">
             <Patients />
           </div>
   );
