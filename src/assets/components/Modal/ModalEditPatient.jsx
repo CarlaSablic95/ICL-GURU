4e561a016b598eefe05ec4ac14910ab5f7fec607
OrnellaSablic <ornellasablic@gmail.com>
2024-06-02 21:15:00 -0300

    add(-): Importación de formulario

--- src/assets/components/Modal/ModalEditPatient.jsx
+++ src/assets/components/Modal/ModalEditPatient.jsx
@@ -1,4 +1,6 @@
 import Button from "../Button/Button";
+import BaseForm from "../BaseForm/BaseForm";
+import { Input, InputRadio } from "../BaseForm/Input";
 
 const ModalEditPatient = () => {
     return (
@@ -10,50 +12,65 @@ const ModalEditPatient = () => {
                     <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div className="modal-body pb-0">
-                    <form className="px-4">
-                    <div className="mb-3">
-                        <label htmlFor="name" className="form-label">Name</label>
-                        <input type="text" className="form-control" id="name" placeholder="Lola" />
-                    </div>
+                        <BaseForm>
+                        <Input 
+                            label="Name"
+                            id="name"
+                            name="name"
+                            type="text"
+                            placeholder="example"
+                            value=""
+                        />
 
-                    <div className="mb-3">
-                        <label htmlFor="lastname" className="form-label">Last name</label>
-                        <input type="text" className="form-control" id="lastname" placeholder="Fiz " />
-                    </div>
+                            <Input 
+                            label="Last name"
+                            id="lastname"
+                            name="lastname"
+                            type="text"
+                            placeholder="example"
+                            value=""
+                        />
 
-                    <div className="mb-3">
-                        <label htmlFor="dob" className="form-label">Date of birth</label>
-                        <input type="date" className="form-control" id="dob" />
-                    </div>
-
-                        <p className="mb-0">Sex</p>
-                        <div className="d-flex justify-content-evenly mb-3">
-                            <div className="form-check">
-                                <label className="form-check-label" htmlFor="flexRadioDefault1">
-                                    Female
-                                </label>
-                        <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1" />
-                        </div>
-
-                        <div className="form-check">
-                            <label className="form-check-label" htmlFor="flexRadioDefault2">
-                            Male
-                            </label>
-                            <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" />
-                        </div>
-                        </div>
-
-                        {/* MRN SE SACA */}
-                        {/* <div className="mb-3">
-                            <label htmlFor="mrn" className="form-label">Medical records number (MRN)</label>
-                            <input type="text" className="form-control" id="mrn" placeholder="45454" />
-                        </div> */}
+                        <Input 
+                            label="Date of birth"
+                            type="date"
+                        />
 
                         <div className="mb-3">
-                            <label htmlFor="patientsID" className="form-label">Patients ID</label>
-                            <input type="text" className="form-control" id="patientsID" placeholder="12012" />
+                            <label className="mb-0">Sex<span className="text-danger">*</span></label>
+                                <div className="d-flex justify-content-evenly">
+                                    <InputRadio 
+                                    label="Female"
+                                    name="sex"
+                                    value="female"
+                                    type="radio"
+                                />
+
+                                <InputRadio 
+                                    label="Male"
+                                    name="sex"
+                                    value="male"
+                                    type="radio"
+                                />
+                                </div>
                         </div>
-                    </form>
+
+                        <Input 
+                            label="Medical records number (MRN)"
+                            id="mrn"
+                            name="mrn"
+                            type="text"
+                            placeholder="00000"
+                        />
+
+                        <Input 
+                            label="Patients ID"
+                            id="id"
+                            name="id"
+                            type="text"
+                            placeholder="00000"
+                        />
+                        </BaseForm>
                     </div>
                     <div className="modal-footer border-0 d-flex justify-content-evenly">
                         <Button type="submit" name="Update" bg_color="#3DC2DD" />
