bf817893a667cbb8d1efd4a2f10a0ba7476b1427
CarlaSablic95 <carlasablic95@gmail.com>
2024-06-03 00:43:49 -0300

    fix(-): Añadí clases de bootstrap para ocultar y mostrar el header en distintas resoluciones

--- src/assets/components/Header/Header.jsx
+++ src/assets/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import Sidebar from "../Sidebar/Sidebar";
 const Header = () => {
     return (
         <>
-            <header className="py-4 d-flex justify-content-between">
+            <header className="py-4 d-flex justify-content-between d-none d-lg-flex">
                 <p className='text-white mx-5'>User:</p>
                 <div className="bg-warning mx-5">Logo</div>
             </header>
