29b9b35cc86f840cad0daed2f45a49695f31e09a
CarlaSablic95 <carlasablic95@gmail.com>
2024-06-07 13:21:05 -0300

    add(-): Creación del componente Boton

--- src/assets/components/Button/Button.jsx
+++ src/assets/components/Button/Button.jsx
@@ -1,6 +1,6 @@
-const Button = ({ name, type, bg_color }) => {
+const Button = ({ title, type, bg_color }) => {
     return ( 
-        <button type={ type } className="btn rounded-3 text-uppercase py-2 px-4 border-0 text-white" style={{ backgroundColor: bg_color }}>{ name }</button>
+        <button type={ type } className="btn rounded-3 text-uppercase py-2 px-4 border-0 text-white" style={{ backgroundColor: bg_color }}>{ title }</button>
     )
 }
 
