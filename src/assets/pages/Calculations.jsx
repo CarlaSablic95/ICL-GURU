4431c7028229135dfda7cb37f307730ae4d113a0
CarlaSablic95 <carlasablic95@gmail.com>
2024-06-07 20:22:26 -0300

    add(-): Importación de botón

--- src/assets/pages/Calculations.jsx
+++ src/assets/pages/Calculations.jsx
@@ -1,8 +1,21 @@
+import ButtonModal from "../components/Button/ButtonModal";
+import CalculationData from "../components/Table/CalculationData";
+
 const Calculations = () => {
     
     return (
-        <div className="col-10 px-5">
-            <h1 className="text-center">Calculations</h1>
+        <div className="col-10 p-5">
+            <div className="d-flex justify-content-evenly align-items-center mb-5">
+                <h1 className="text-center">Calculations</h1>
+                <ButtonModal title="New calculations" icon="./icons/add-user.svg" />
+            </div>
+            <div className="mb-5">
+                <p className="fw-bold mb-1">Patient:</p>
+                <p className="fw-bold text-uppercase mb-1">Dob:</p>
+                <p className="fw-bold mb-1">Organization:</p>
+            </div>
+
+            <CalculationData />
         </div>
     )
 }
