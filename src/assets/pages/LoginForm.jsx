57b3942ece18b801be977b30c08664e89c3f7756
CarlaSablic95 <carlasablic95@gmail.com>
2024-07-07 15:06:20 -0300

    drop(-): Eliminé el input email del formulario

--- src/assets/pages/LoginForm.jsx
+++ src/assets/pages/LoginForm.jsx
@@ -17,55 +17,33 @@ const LoginForm = () => {
     formState: { errors },
   } = useForm();
   const onSubmit = (data) => {
-    const { username, email, password } = data;
+    const { username, password } = data;
         console.log("Username:", username);
-        console.log("Email:", email);
         console.log("Password:", password);
   };
 
-  const isFormInvalid = !!errors.username || !!errors.email || !!errors.password;
+  const isFormInvalid = !!errors.username || !!errors.password;
 
   return (
       <section className="bg-login d-flex  align-items-center">
         <div className="container">
             <div className="row justify-content-center form-login">
-                <div className="col-10 col-md-4 bg-fondo d-none d-md-block"></div>
-                    <div className="col-10 col-md-4 px-5 px-md-4 bg-form">
-                        <form onSubmit={handleSubmit(onSubmit)}>
+                <div className="col-10 col-md-5 bg-fondo d-none d-md-block"></div>
+                    <div className="col-10 col-md-5 px-5 px-md-4 bg-form d-flex flex-column justify-content-center">
+                        <form onSubmit={handleSubmit(onSubmit)} className="px-3 px-md-5">
                             <h1 className="text-uppercase text-center py-4 text-white">Icl Guru</h1>
-                        <div className="form-floating mb-3 text-center">
+                        <div className="form-floating mb-4 text-center">
                             <input
                             {...register("username", { required: true })}
                             placeholder="Username"
-                            className={`form-control rounded-5 border-2 ${errors.username ? "border border-danger" : ""} `}
+                            className={`form-control rounded-5 ${errors.username ? "border border-2 border-danger" : ""} `}
                             id="username"
                             type="text"
                             />
                             {errors.username && (
-                            <small className="text-danger">This field is required</small>
+                            <small className="text-danger fw-bold">This field is required</small>
                             )}
                             <label htmlFor="username">Username</label>
-                        </div>
-
-                        <div className="form-floating mb-3 text-center">
-                            <input
-                            {...register("email", {
-                                required: true,
-                                pattern: /^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
-                            })}
-                            placeholder="Email"
-                            className={`form-control rounded-5 border-2 ${errors.email ? "border border-danger" : ""} `}
-                            id="email"
-                            type="email"
-                            />
-                            {errors.email && (
-                            <small className="text-danger">
-                                { errors.email.type === "required" && "This field is required" }
-                                { errors.email.type === "pattern" && "Invalid email format" }
-                            </small>
-
-                            )}
-                            <label htmlFor="email">Email</label>
                         </div>
 
                         <div className="form-floating mb-3 text-center">
@@ -78,13 +56,13 @@ const LoginForm = () => {
                             <input
                             {...register("password", { required: true, minLength: 8, maxLength: 8 })}
                             placeholder="Password"
-                            className={`form-control rounded-5 border-2 ${errors.password ? "border border-danger" : ""} `}
+                            className={`form-control rounded-5 ${errors.password ? "border border-2 border-danger" : ""} `}
                             id="password"
                             type={showPassword ? "text" : "password"}
                             />
 
                             {errors.password && (
-                            <small className="text-danger">
+                            <small className="text-danger fw-bold">
                                 { errors.password.type === "required" && "This field is required"}
                                 { errors.password.type === "minLength" && "Must be at least 8 characters"}
                                 { errors.password.type === "maxLength" && "Must be 8 characters"}
@@ -93,7 +71,7 @@ const LoginForm = () => {
                             <label htmlFor="password">Password</label>
                         </div>
                         <div className="d-flex justify-content-center">
-                            <Button title="Login" type="submit" bgColor="#00507C" textColor="#fefefe" textTransform="uppercase" rounded="3rem" disabled={ !isFormInvalid } />
+                            <Button title="Login" type="submit" bgColor="#00507C" textColor="#fefefe" textTransform="uppercase" rounded="3rem" width="220px" height="50px" disabled={ !isFormInvalid } />
                         </div>
 
                         <p className="text-center py-3"><a href="#" className="text-white text-decoration-none">I forgot my password</a></p>
